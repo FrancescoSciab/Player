6094fd1a2107e32bb3fca4e4f13281a615e47353
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2024-06-27 18:55:47 +0200

    integrating file to get data from

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import { Typeahead } from "react-bootstrap-typeahead";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import Form from "react-bootstrap/Form";
 
 export default function SearchBar() {
+  const [data, setData] = useState([]);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    fetch("../../public/data.json")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+  console.log(data);
+  // Bypass client-side filtering by returning `true`. Results are already
+  // filtered by the search endpoint, so no need to do it again.
+  const filterBy = () => true;
   return (
     <>
       <Form.Group>
@@ -10,8 +24,9 @@ export default function SearchBar() {
         <Typeahead
           id="basic-typeahead-single"
           labelKey="name"
+          filterBy={filterBy}
           //   onChange={setSingleSelections}
-          //   options={options}
+          options={options}
           placeholder="Search a song..."
           //   selected={singleSelections}
         />
