db4f7db028370263052cac8ea137080a851ba913
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2024-06-28 12:06:06 +0200

    flattening nested data structure from data.jsom, working on Typeahead component to filter by band name, album title, or song title

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,33 +1,75 @@
-import { useEffect, useState } from "react";
-import { Typeahead } from "react-bootstrap-typeahead";
+import { useState } from "react";
+import { AsyncTypeahead, Typeahead } from "react-bootstrap-typeahead";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import Form from "react-bootstrap/Form";
 
 export default function SearchBar() {
-  const [data, setData] = useState([]);
+  //   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [options, setOptions] = useState([]);
 
-  useEffect(() => {
+  const handleSearch = (query) => {
+    setIsLoading(true);
+
     fetch("/data.json")
       .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+      .then((data) => {
+        const flattenedData = data.flatMap((band) =>
+          band.albums.flatMap((album) =>
+            album.songs.map((song) => ({
+              bandName: band.title,
+              albumTitle: album.title,
+              songTitle: song.title,
+              songLength: song.length,
+            }))
+          )
+        );
+        // console.log(`albums: ${albums}`);
+
+        setOptions(flattenedData);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
+  };
+
   // Bypass client-side filtering by returning `true`. Results are already
   // filtered by the search endpoint, so no need to do it again.
   const filterBy = () => true;
+
+  const renderMenuItemChildren = (option) => {
+    return (
+      <>
+        <span>
+          <strong>{option.songTitle}</strong> by <em>{option.bandName}</em>
+        </span>
+        <div>
+          <small>Album: {option.albumTitle}</small>
+        </div>
+        <div>
+          <small>Length: {option.songLength}</small>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <Form.Group>
         <Form.Label>Search your song</Form.Label>
-        <Typeahead
+
+        <AsyncTypeahead
           id="basic-typeahead-single"
-          labelKey="name"
+          labelKey="songTitle"
           filterBy={filterBy}
-          //   onChange={setSingleSelections}
+          isLoading={isLoading}
+          onSearch={handleSearch}
           options={options}
           placeholder="Search a song..."
           //   selected={singleSelections}
+          renderMenuItemChildren={renderMenuItemChildren}
         />
       </Form.Group>
     </>
