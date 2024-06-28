834bccc5cd7d5593227cafb7bbacae256b59f388
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2024-06-28 18:35:00 +0200

    search algorithm working. Starting css

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,32 +1,74 @@
-import { useEffect, useState } from "react";
-import { Typeahead } from "react-bootstrap-typeahead";
+import { useState } from "react";
+import { Highlighter, Typeahead } from "react-bootstrap-typeahead";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import Form from "react-bootstrap/Form";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SearchBar() {
-  const [data, setData] = useState([]);
+  //   const [data, setData] = useState([]);
   const [options, setOptions] = useState([]);
+  const props = {};
 
-  useEffect(() => {
+  const handleChange = () => {
     fetch("/data.json")
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        //Library search algorithms need flattened data
+        const flattenedData = [];
+        data.forEach((artist) => {
+          artist.albums.forEach((album) => {
+            album.songs.forEach((song) => {
+              flattenedData.push({
+                artistName: artist.name,
+                albumTitle: album.title,
+                songTitle: song.title,
+                songLength: song.length,
+              });
+            });
+          });
+        });
+        setOptions(flattenedData);
+      })
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-  // Bypass client-side filtering by returning `true`. Results are already
-  // filtered by the search endpoint, so no need to do it again.
-  const filterBy = () => true;
+  };
+
+  const labelKey = (option) =>
+    `${option.songTitle} - ${option.artistName} (${option.albumTitle})`;
+
+  //It is the dropdown menu
+  props.renderMenuItemChildren = (option, { text }) => (
+    <>
+      <Highlighter search={text}>{option.songTitle}</Highlighter>
+
+      <div>
+        <small>
+          by <Highlighter search={text}>{option.artistName}</Highlighter> from
+          the album
+          <Highlighter search={text}>{option.albumTitle}</Highlighter>
+        </small>
+      </div>
+      <div>
+        <small>Length: {option.songLength}</small>
+      </div>
+    </>
+  );
+
   return (
     <>
-      <Form.Group>
+      <Form.Group className="mx-5">
         <Form.Label>Search your song</Form.Label>
         <Typeahead
+          {...props}
           id="basic-typeahead-single"
-          labelKey="name"
-          filterBy={filterBy}
-          //   onChange={setSingleSelections}
+          labelKey={labelKey}
+          filterBy={["albumTitle", "artistName", "songLength", "songTitle"]}
+          onInputChange={handleChange}
           options={options}
           placeholder="Search a song..."
+          clearButton="true"
+          size="lg"
+          autoFocus="true"
           //   selected={singleSelections}
         />
       </Form.Group>
